c83039265bf96d1397de42a6ba52d0bd78ccf348
Burdzi0 <burdzi0@programmer.net>
2018-11-27 18:44:45 +0100

    Git init

---
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/LabdaybackendspringApplication.java
@@ -0,0 +1,12 @@
+package pl.wroclaw.asi.labdaybackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabdaybackendspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabdaybackendspringApplication.class, args);
+	}
+}
