0defde4ef92b9604722e60d183f832fe0eabb691
jBosak98 <32513817+jBosak98@users.noreply.github.com>
2018-12-18 20:07:17 +0100

    Timetable model (#5)
    * timetable model created

---
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/model/Timetable.java
@@ -0,0 +1,33 @@
+package pl.wroclaw.asi.labdaybackendspring.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Data
+@Builder
+public class Timetable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("path_id")
+    @NonNull
+    private int pathId;
+
+    @JsonProperty("event_id")
+    private int eventId;
+
+    @JsonProperty("time_start")
+    private int timeStart;
+
+    @JsonProperty("time_end")
+    private int timeEnd;
+}
