48f5ab59559aa5078f4af32183f111903bd22dd7
jBosak98 <jbosak98@gmail.com>
2018-12-18 19:29:31 +0100

    deleted code in main

--- src/main/java/pl/wroclaw/asi/labdaybackendspring/LabdaybackendspringApplication.java
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/LabdaybackendspringApplication.java
@@ -1,27 +1,12 @@
-package pl.wroclaw.asi.labdaybackendspring
+package pl.wroclaw.asi.labdaybackendspring;
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import pl.wroclaw.asi.labdaybackendspring.model.Timetable
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-object LabdaybackendspringApplication {
+public class LabdaybackendspringApplication {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        SpringApplication.run(LabdaybackendspringApplication::class.java, *args)
-        val t1 = Timetable(4, 2, 5, 1521882000, 1521885600)
-        val t2 = Timetable
-                .builder()
-                .id(4)
-                .pathId(2)
-                .eventId(5)
-                .timeStart(1521882000)
-                .timeEnd(1521885600)
-                .build()
-        assert(t1.id == 4)
-        assert(t1 === t2)
-        t1.pathId = 1
-        assert(t1.pathId == 1)
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LabdaybackendspringApplication.class, args);
+	}
 }
