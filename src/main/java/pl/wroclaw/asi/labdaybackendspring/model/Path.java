9b76068b0c3d2133e1c4485708c4f2fdb5f94b5d
Patryk Szwed <32751479+patrykszwed@users.noreply.github.com>
2018-12-30 22:48:34 +0100

    Added Speaker.java (#9)
    * Renamed Paths.java to Path.java, changed id to int and added NoArgsConstructor annotation
    
    * Added Event class
    
    * Added Speaker model

---
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/model/Path.java
@@ -0,0 +1,30 @@
+package pl.wroclaw.asi.labdaybackendspring.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Path {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("info")
+    private String info;
+
+    @JsonProperty("active")
+    private boolean active;
+}
--- src/main/java/pl/wroclaw/asi/labdaybackendspring/model/Speaker.java
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/model/Speaker.java
@@ -2,6 +2,7 @@ package pl.wroclaw.asi.labdaybackendspring.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,13 +10,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
 @Data
-public class Paths {
+public class Speaker {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("id")
-    private Long id;
+    private int id;
 
     @JsonProperty("name")
     private String name;
@@ -23,6 +25,6 @@ public class Paths {
     @JsonProperty("info")
     private String info;
 
-    @JsonProperty("active")
-    private boolean active;
+    @JsonProperty("img")
+    private String img;
 }
