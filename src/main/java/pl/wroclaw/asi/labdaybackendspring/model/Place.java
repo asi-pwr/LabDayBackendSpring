47da2b2f58b447825fae2fe8e54ff5b6ae476d39
Patryk Szwed <patryk.szwed@outlook.com>
2019-01-04 03:06:08 +0100

    Added model for Places

---
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/model/Place.java
@@ -0,0 +1,39 @@
+package pl.wroclaw.asi.labdaybackendspring.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("type")
+    private int type;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("info")
+    private String info;
+
+    @JsonProperty("img")
+    private String img;
+
+    @JsonProperty("latitude")
+    private String latitude;
+
+    @JsonProperty("longitude")
+    private String longitude;
+}
