1bae046e459718381072f27ce93309b95fe0f52a
jBosak98 <jbosak98@gmail.com>
2019-01-18 19:10:59 +0100

    add,delete, getAll methods in Controllers

--- src/main/java/pl/wroclaw/asi/labdaybackendspring/controllers/PathController.java
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/controllers/PathController.java
@@ -1,4 +1,52 @@
 package pl.wroclaw.asi.labdaybackendspring.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.wroclaw.asi.labdaybackendspring.model.Path;
+import pl.wroclaw.asi.labdaybackendspring.services.PathService;
+import pl.wroclaw.asi.labdaybackendspring.services.ValidationErrorService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/api/path")
 public class PathController {
+
+    private final PathService pathService;
+    private final ValidationErrorService validationErrorService;
+
+    @Autowired
+    public PathController(PathService pathService, ValidationErrorService validationErrorService) {
+        this.pathService = pathService;
+        this.validationErrorService = validationErrorService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addPath(@Valid @RequestBody Path path, BindingResult result){
+
+        ResponseEntity<?> errors = validationErrorService.mapValidationService(result);
+
+        if(errors != null)
+            return errors;
+        pathService.saveOrUpdatePath(path);
+        return new ResponseEntity<>("", HttpStatus.CREATED);
+
+    }
+
+    @DeleteMapping("/delete/{pathId}")
+    public ResponseEntity<?> deletePathById(@PathVariable Integer PathId){
+        pathService.deletePath(PathId);
+        return new ResponseEntity<>("Path with id: " + PathId + "was successfully deleted", HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity getAllPaths(){
+        return new ResponseEntity<>(pathService.findAllPaths(),HttpStatus.OK);
+    }
+
 }
--- src/main/java/pl/wroclaw/asi/labdaybackendspring/controllers/TimetableController.java
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/controllers/TimetableController.java
@@ -1,4 +1,49 @@
 package pl.wroclaw.asi.labdaybackendspring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.wroclaw.asi.labdaybackendspring.model.Timetable;
+import pl.wroclaw.asi.labdaybackendspring.services.TimetableService;
+import pl.wroclaw.asi.labdaybackendspring.services.ValidationErrorService;
+
+import javax.validation.Valid;
+
+@Service
+@RequestMapping("/api/timetable")
 public class TimetableController {
+    private final TimetableService timetableService;
+    private final ValidationErrorService validationErrorService;
+
+    @Autowired
+    public TimetableController(TimetableService timetableService, ValidationErrorService validationErrorService) {
+        this.timetableService = timetableService;
+        this.validationErrorService = validationErrorService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addTimetable(@Valid @RequestBody Timetable timetable, BindingResult result){
+
+        ResponseEntity<?> errors = validationErrorService.mapValidationService(result);
+
+        if(errors != null)
+            return errors;
+        timetableService.saveOrUpdateTimetable(timetable);
+        return new ResponseEntity<>("", HttpStatus.CREATED);
+
+    }
+
+    @DeleteMapping("/delete/{timetableId}")
+    public ResponseEntity<?> deleteTimetableById(@PathVariable Integer timetableId){
+        timetableService.deleteTimetable(timetableId);
+        return new ResponseEntity<>("Timetable with id: " + timetableId + "was successfully deleted", HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity getAllTimetables(){
+        return new ResponseEntity<>(timetableService.findAllTimetables(),HttpStatus.OK);
+    }
 }
