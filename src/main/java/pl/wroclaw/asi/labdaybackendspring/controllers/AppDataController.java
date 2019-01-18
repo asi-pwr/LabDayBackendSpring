07b60b2edbcb309f7a7919b0b858ffd22f3904f3
jBosak98 <jbosak98@gmail.com>
2019-01-18 18:28:54 +0100

    appDataController

--- src/main/java/pl/wroclaw/asi/labdaybackendspring/controllers/AppDataController.java
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/controllers/AppDataController.java
@@ -3,42 +3,49 @@ package pl.wroclaw.asi.labdaybackendspring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import pl.wroclaw.asi.labdaybackendspring.model.Event;
-import pl.wroclaw.asi.labdaybackendspring.services.EventService;
-import pl.wroclaw.asi.labdaybackendspring.services.ValidationErrorService;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.wroclaw.asi.labdaybackendspring.model.AppData;
+import pl.wroclaw.asi.labdaybackendspring.services.*;
 
 @RestController
-@RequestMapping()//TODO
-@CrossOrigin
-public class EventController {
+@RequestMapping("/api")
+public class AppDataController {
 
     private final EventService eventService;
+    private final PlaceService placeService;
+    private final PathService pathService;
+    private final TimetableService timetableService;
+    private final SpeakerService speakerService;
+
     private final ValidationErrorService validationErrorService;
 
     @Autowired
-    public EventController(EventService eventService, ValidationErrorService validationErrorService) {
+    public AppDataController(EventService eventService, PlaceService placeService, PathService pathService, TimetableService timetableService, SpeakerService speakerService, ValidationErrorService validationErrorService) {
         this.eventService = eventService;
+        this.placeService = placeService;
+        this.pathService = pathService;
+        this.timetableService = timetableService;
+        this.speakerService = speakerService;
         this.validationErrorService = validationErrorService;
     }
 
-
-
-    @PostMapping("/add")
-    public ResponseEntity<?> createNewEvent(
-            @Valid @RequestBody Event event,
-            BindingResult result){
-        ResponseEntity<?> errors = validationErrorService.mapValidationService(result);
-
-        if (errors != null)
-            return errors;
-
-        Event tmp = eventService.saveOrUpdateEvent(event);
-        return new ResponseEntity<>(tmp, HttpStatus.CREATED);
+    @RequestMapping(value = "/app-data", method = RequestMethod.GET)
+    public ResponseEntity<?> getAppData(){
+        AppData appData = new AppData(
+                eventService.findAllEvents(),
+                placeService.findAllPlaces(),
+                pathService.findAllPaths(),
+                timetableService.findAllTimetables(),
+                speakerService.findAllSpeakers()
+        );
+        return new ResponseEntity<>(appData, HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @RequestMapping(value = "/last-update")
+    public ResponseEntity<?> getLastUpdate(){
+        //LastUpdate lastUpdate = new LastUpdate(ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT));
+        return new ResponseEntity<>("What is last-update?", HttpStatus.OK);
+    }
 }
