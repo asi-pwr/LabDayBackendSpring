b4b413f5c351aca0233f5294cd3efa12c2fd1722
jBosak98 <jbosak98@gmail.com>
2019-01-18 18:28:14 +0100

    models to rest requests

--- src/main/java/pl/wroclaw/asi/labdaybackendspring/model/AppData.java
+++ src/main/java/pl/wroclaw/asi/labdaybackendspring/model/AppData.java
@@ -1,4 +1,30 @@
 package pl.wroclaw.asi.labdaybackendspring.model;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
 public class AppData {
+
+    @JsonProperty("events")
+    private List<Event> events;
+
+    @JsonProperty("map_others")
+    private List<Place> mapOthers;
+
+    @JsonProperty("paths")
+    private List<Path> paths;
+
+    @JsonProperty("timetables")
+    private List<Timetable> timetables;
+
+    @JsonProperty("speakers")
+    private List<Speaker> speakers;
+
+
 }
