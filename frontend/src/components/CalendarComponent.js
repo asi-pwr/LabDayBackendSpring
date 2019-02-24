d3e2cb933ef80a1c864f6624bc2be0cee4ecf56d
jBosak98 <jbosak98@gmail.com>
2019-02-24 11:44:04 +0100

    theme and first appointment

--- frontend/src/components/CalendarComponent.js
+++ frontend/src/components/CalendarComponent.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Paper from "@material-ui/core/es/Paper/Paper";
-import {DayView, Scheduler} from '@devexpress/dx-react-scheduler-material-ui';
+import {Appointments, DayView, Scheduler, WeekView} from '@devexpress/dx-react-scheduler-material-ui';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import {blue} from "@material-ui/core/colors";
+
+const theme = createMuiTheme(
+    {
+        typography: { useNextVariants: true },
+        palette: { type: "light", primary: blue }
+    });
+
+
 
 class CalendarComponent extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            data:[]
+            data:[
+                {
+                    startDate: '2019-02-24 15:30',
+                    endDate: '2019-02-24 16:00',
+                    title: 'Rejestracja'
+                }
+            ]
         }
 
     }
@@ -17,11 +34,14 @@ class CalendarComponent extends React.Component {
             <div>
                 CalendarComponent
                 <br/>
+                <MuiThemeProvider theme={theme}>
                 <Paper>
                     <Scheduler data={data}>
                         <DayView/>
+                        <Appointments />
                     </Scheduler>
                 </Paper>
+                </MuiThemeProvider>
 
             </div>
         )
