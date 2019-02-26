6aef39bb9f15e6b2f13f8a68410e608ac0933e90
jBosak98 <jbosak98@gmail.com>
2019-02-26 21:13:42 +0100

    newPath button init

--- frontend/src/calendar/ToolbarCalendarComponent.js
+++ frontend/src/calendar/ToolbarCalendarComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Toolbar } from '@devexpress/dx-react-scheduler-material-ui';
 import PathSelectorComponent from "./PathSelectorComponent";
 import {withStyles} from "@material-ui/core";
+import Button from "@material-ui/core/es/Button/Button";
 
 
 class ToolbarCalendarComponent extends React.Component {
@@ -9,7 +10,15 @@ class ToolbarCalendarComponent extends React.Component {
         const { path, pathChange, paths, classes } = this.props
         return (
             <Toolbar.FlexibleSpace className={classes.flexibleSpace}>
+                <Button
+                    className={classes.button}
+                    variant="outlined"
+                    color="primary">
+                    nowa ścieżka
+                </Button>
+
                 <PathSelectorComponent path={path} pathChange={pathChange} paths={paths}/>
+
             </Toolbar.FlexibleSpace>
         )
     }
@@ -18,6 +27,11 @@ class ToolbarCalendarComponent extends React.Component {
 const styles = theme => ({
     flexibleSpace: {
         margin: 'auto 40px auto auto',
+        flex: 'inherit'
     },
+    button: {
+        marginLeft: theme.spacing.unit * 3,
+
+    }
 });
 export default withStyles(styles)(ToolbarCalendarComponent)
