492c9bb9966561aa749a0bd612b3f7613bac7ac2
jBosak98 <jbosak98@gmail.com>
2019-03-09 13:55:12 +0100

    PublicAccessActiveRespository

---
+++ backend/src/main/java/pl/wroclaw/asi/labdaybackendspring/repositories/PublicAccessActiveRepository.java
@@ -0,0 +1,9 @@
+package pl.wroclaw.asi.labdaybackendspring.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.wroclaw.asi.labdaybackendspring.model.PublicAccessActive;
+
+
+public interface PublicAccessActiveRepository extends CrudRepository<PublicAccessActive, Integer> {
+
+}
