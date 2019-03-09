fc626df3d6738acef63b07f17a05760a61d15a13
jBosak98 <jbosak98@gmail.com>
2019-03-09 15:59:24 +0100

    redux tools turned off

--- frontend/src/helpers/store.js
+++ frontend/src/helpers/store.js
@@ -8,6 +8,6 @@ export const store = createStore(
     rootReducer,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
