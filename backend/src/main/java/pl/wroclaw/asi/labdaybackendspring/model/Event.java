ed7f58b07b95c75144130fd5bac7eb11abdf90cd
jBosak98 <jbosak98@gmail.com>
2019-03-10 11:10:51 +0100

    Lob added to info

--- backend/src/main/java/pl/wroclaw/asi/labdaybackendspring/model/Event.java
+++ backend/src/main/java/pl/wroclaw/asi/labdaybackendspring/model/Event.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
@@ -31,6 +28,7 @@ public class Event {
     @JsonProperty("room")
     private String room;
 
+    @Lob
     @JsonProperty("info")
     private String info;
 
