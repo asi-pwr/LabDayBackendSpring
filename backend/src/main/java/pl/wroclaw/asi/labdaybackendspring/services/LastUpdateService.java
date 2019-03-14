4a73957601a08f9c9b6ed655b59cc9e248124db9
jBosak98 <jbosak98@gmail.com>
2019-03-14 21:13:33 +0100

    unused code removed

--- backend/src/main/java/pl/wroclaw/asi/labdaybackendspring/services/LastUpdateService.java
+++ backend/src/main/java/pl/wroclaw/asi/labdaybackendspring/services/LastUpdateService.java
@@ -1,7 +1,4 @@
 package pl.wroclaw.asi.labdaybackendspring.services;
 
-import pl.wroclaw.asi.labdaybackendspring.model.LastUpdate;
 
-public interface LastUpdateService {
-//    public LastUpdate getLastUpdate();
-}
+public interface LastUpdateService { }
